976b9b9a2bfc3ea7ab5105a3a5ba7c38a665d3f8
Bill-Park <tkddn0916@gmail.com>
2024-06-07 22:51:51 +0900

    generator setup

--- src/generate.ts
+++ src/generate.ts
@@ -1,8 +1,13 @@
 import { Options, ParseSchemaType, SchemaOutputType } from "./types"
 import SwaggerParser from "@apidevtools/swagger-parser"
-import { faker } from "@faker-js/faker"
+import { Faker, ko } from "@faker-js/faker"
 import { isReference } from "oazapfts/generate"
 import { OpenAPIV3_1 } from "openapi-types"
+
+const faker = new Faker({
+  locale: [ko],
+})
+faker.seed(1)
 
 const defaultOptions: Options = {
   path: "",
@@ -14,6 +19,15 @@ const defaultOptions: Options = {
 
 const ARRAY_MIN_LENGTH = 1
 const ARRAY_MAX_LENGTH = 3
+
+const MIN_STRING_LENGTH = 3
+const MAX_STRING_LENGTH = 20
+
+const MIN_INTEGER = 1
+const MAX_INTEGER = 100000
+
+const MIN_NUMBER = 0
+const MAX_NUMBER = 100
 
 const getRandomLengthArray = (min: number = ARRAY_MIN_LENGTH, max: number = ARRAY_MAX_LENGTH) => {
   const length = Math.floor(Math.random() * (max - min + 1)) + min
@@ -90,7 +104,12 @@ const parseSchema = (
     )
   } else if (schemaValue.oneOf !== undefined) {
     // oneOf value, exactly one. Can't find example
-    return
+    const oneOfValue = schemaValue.oneOf
+    return faker.helpers.arrayElement(
+      oneOfValue.map((field) => {
+        return parseSchema(field, outputSchema)
+      })
+    )
   } else if (schemaValue.type === "array") {
     // array
     const arrayValue = schemaValue.items
@@ -99,40 +118,31 @@ const parseSchema = (
       | Record<string, SchemaOutputType>
     )[]
   } else if (schemaValue.type === "string" && schemaValue.format === "date-time") {
-    // date-time
-    return "datetime"
+    // date-time, 2017-07-21T17:32:28Z
+    return valueGenerator(schemaValue)
   } else if (schemaValue.type === "string" && schemaValue.format === "date") {
-    // date
-    return "date"
+    // date, 2017-07-21
+    return valueGenerator(schemaValue)
   } else if (schemaValue.type === "string" && schemaValue.title?.toLowerCase() === "b64uuid") {
     //b64uuid
-    return "b64uuid"
+    return valueGenerator(schemaValue)
   } else if (schemaValue.type === "string" && schemaValue.maxLength) {
     // string has max length
-    return "maxlength"
+    return valueGenerator(schemaValue)
   } else if (schemaValue.type === "string" && schemaValue.minLength) {
     // string has min length
-    return "min len"
+    return valueGenerator(schemaValue)
   } else if (schemaValue.type === "string" && schemaValue.pattern) {
     // regex pattern string
     return "pattern string"
   } else if (schemaValue.type === "string") {
-    return "str"
+    return valueGenerator(schemaValue)
   } else if (schemaValue.type === "integer") {
     // integer value.
-    return 1
-  } else if (schemaValue.type === "number" && schemaValue.maximum && schemaValue.minimum) {
-    // numbers include decimal with min and max
-    return 2.2
-  } else if (schemaValue.type === "number" && schemaValue.maximum) {
-    // numbers include decimal with max
-    return 3.3
-  } else if (schemaValue.type === "number" && schemaValue.minimum) {
-    // numbers include decimal with min
-    return 0.1
+    return valueGenerator(schemaValue)
   } else if (schemaValue.type === "number") {
-    // numbers include decimal
-    return 0.1
+    // numbers include decimal with min and max
+    return valueGenerator(schemaValue)
   } else if (schemaValue.type === "boolean") {
     return true
   } else if (schemaValue.type === "null") {
@@ -142,4 +152,91 @@ const parseSchema = (
     return "any"
   }
   return
+}
+
+const uuidToB64 = (uuid: string) => {
+  const uuidBuffer = Buffer.from(uuid.replace(/-/g, ""), "hex")
+  const base64Uuid = uuidBuffer
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "")
+  return base64Uuid
+}
+
+const valueGenerator = (schemaValue: OpenAPIV3_1.SchemaObject): ParseSchemaType => {
+  if (schemaValue.type === "string" && schemaValue.format === "date-time") {
+    // date-time, 2017-07-21T17:32:28Z
+    return faker.date
+      .between({
+        from: "2020-01-01T00:00:00.000Z",
+        to: "2030-12-31T23:59:59.999Z",
+      })
+      .toISOString()
+  } else if (schemaValue.type === "string" && schemaValue.format === "date") {
+    // date, 2017-07-21
+    return faker.date
+      .between({
+        from: "2020-01-01T00:00:00.000Z",
+        to: "2030-12-31T23:59:59.999Z",
+      })
+      .toISOString()
+      .split("T")[0]
+  } else if (schemaValue.type === "string" && schemaValue.title?.toLowerCase() === "b64uuid") {
+    // generate base 64 uuid
+    const baseUuid = faker.string.uuid()
+    return uuidToB64(baseUuid)
+  } else if (schemaValue.type === "string" && schemaValue.minLength && schemaValue.maxLength) {
+    // string has max length
+    return faker.string.alphanumeric({
+      length: { min: schemaValue.minLength, max: schemaValue.maxLength },
+    })
+  } else if (schemaValue.type === "string" && schemaValue.maxLength) {
+    // string has max length
+    const minStringLength = Math.min(MIN_STRING_LENGTH, schemaValue.maxLength)
+    return faker.string.alphanumeric({
+      length: { min: minStringLength, max: schemaValue.maxLength },
+    })
+  } else if (schemaValue.type === "string" && schemaValue.minLength) {
+    // string has min length
+    const maxStringLength = Math.max(MAX_STRING_LENGTH, schemaValue.minLength)
+    return faker.string.alphanumeric({
+      length: { min: schemaValue.minLength, max: maxStringLength },
+    })
+  } else if (schemaValue.type === "string") {
+    return faker.string.alphanumeric({
+      length: { min: MIN_STRING_LENGTH, max: MAX_STRING_LENGTH },
+    })
+  } else if (schemaValue.type === "integer") {
+    return faker.number.int({ min: MIN_INTEGER, max: MAX_INTEGER })
+  } else if (schemaValue.type === "number" && schemaValue.maximum && schemaValue.minimum) {
+    return faker.number.float({
+      min: schemaValue.minimum,
+      max: schemaValue.maximum,
+      fractionDigits: 2,
+    })
+  } else if (schemaValue.type === "number" && schemaValue.maximum) {
+    return faker.number.float({
+      min: MIN_NUMBER,
+      max: schemaValue.maximum,
+      fractionDigits: 2,
+    })
+  } else if (schemaValue.type === "number" && schemaValue.minimum) {
+    return faker.number.float({
+      min: schemaValue.minimum,
+      max: MAX_NUMBER,
+      fractionDigits: 2,
+    })
+  } else if (schemaValue.type === "number") {
+    return faker.number.float({
+      min: MIN_NUMBER,
+      max: MAX_NUMBER,
+      fractionDigits: 2,
+    })
+  } else if (schemaValue.type === "boolean") {
+    return faker.datatype.boolean()
+  } else if (schemaValue.type === "null") {
+    return null
+  }
+  return faker.word.adjective()
 }
