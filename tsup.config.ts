87cf1ab5cc11d933b8f0bc91c26008c478e41354
Bill-Park <tkddn0916@gmail.com>
2024-06-07 22:51:51 +0900

    setup npm deploy

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from "tsup";
+import { Options, defineConfig } from "tsup"
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
-  dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-});
+export default defineConfig((options) => {
+  const commonOptions: Partial<Options> = {
+    entry: ["src/index.ts"],
+    dts: true, // Generate declaration file (.d.ts)
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+  }
+
+  return [
+    {
+      ...commonOptions,
+      format: ["cjs"],
+      target: "es5",
+    },
+    {
+      ...commonOptions,
+      format: ["esm"],
+      target: "es6",
+    },
+  ]
+})
