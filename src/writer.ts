e8cd3f98d845383ef5a3322fd171b4912bb57cd3
Bill-Park <tkddn0916@gmail.com>
2024-06-07 22:51:51 +0900

    setup response writer

--- src/writer.ts
+++ src/writer.ts
@@ -1,5 +1,6 @@
 import { Options, PathNormalizedType } from "./types"
 import { pascalCase, camelCase } from "change-case-all"
+import { isReference } from "oazapfts/generate"
 
 export const writeApi = async (paths: PathNormalizedType[], options: Options) => {
   const mockHandlers = paths
@@ -44,3 +45,37 @@ export const writeApi = async (paths: PathNormalizedType[], options: Options) =>
       return [...codeBaseArray, `})`].join("\n")
     })
     .join("\n\n")
+
+export const writeResponse = async (paths: PathNormalizedType[], options: Options) => {
+  const r = paths
+    .map((path) => {
+      const pathResponses = path.responses.map((res) => {
+        const codeBaseArray = [
+          `export const get${pascalCase(path.operationId)}${res.statusCode} = () => {`,
+        ]
+        if (res.schema?.type === "ref") {
+          const schemaName = camelCase(res.schema.value.$ref.replace("#/components/schemas/", ""))
+          codeBaseArray.push(`  return ${schemaName}()`)
+        } else if (res.schema?.type === "array") {
+          if (isReference(res.schema.value)) {
+            const schemaName = camelCase(res.schema.value.$ref.replace("#/components/schemas/", ""))
+            //Todo: 가져오는게 아닌 생성하는 로직 필요
+            codeBaseArray.push(`  return [${schemaName}()]`)
+          }
+        } else if (res.schema?.type === "anyOf") {
+          const firstSchema = res.schema.value.anyOf?.[0]
+          if (isReference(firstSchema)) {
+            const schemaName = camelCase(firstSchema.$ref.replace("#/components/schemas/", ""))
+            codeBaseArray.push(`  return ${schemaName}()`)
+          } else {
+            codeBaseArray.push(`  return ${res.schema.value}`)
+          }
+        } else {
+          codeBaseArray.push(`  return ${res.schema?.value}`)
+        }
+        return [...codeBaseArray, `}`].join("\n")
+      })
+      return `// ${path.operationId}\n` + pathResponses.join("\n\n")
+    })
+    .join("\n\n")
+}
