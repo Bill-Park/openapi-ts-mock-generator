b1dd56053b9d294c60c757e3f77ac26aa9f43c4c
Bill-Park <tkddn0916@gmail.com>
2024-06-07 22:51:51 +0900

    update main to export function

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,12 @@ import { generateAPI, generateSchema } from "./generate"
 import { Options } from "./types"
 import { writeHandlers, writeResponses, writeSchema } from "./writer"
 
-async function main() {
-  const options: Options = {
-    ...defaultOptions,
-    path: "resources/openapi.json", // file path or url
-    static: true,
-    baseDir: "resources",
-    // includeCodes: [200, 201, 202, 204],
-  }
+export const main = async (options: Options) => {
   const generatedAPI = await generateAPI(options)
   if (generatedAPI === undefined) {
     console.warn("generate api fail")
     return
   }
-  writeHandlers(generatedAPI, options)
-  writeResponses(generatedAPI, options)
 
   const generatedSchema = await generateSchema(options)
   if (generatedSchema === undefined) {
@@ -25,6 +16,15 @@ async function main() {
     return
   }
   writeSchema(generatedSchema, options)
+  writeHandlers(generatedAPI, options)
+  writeResponses(generatedAPI, options)
 }
 
-main()
+const options: Options = {
+  ...defaultOptions,
+  path: "openapi.json", // file path or url
+  static: true,
+  baseDir: "resources",
+  // includeCodes: [200, 201, 202, 204],
+}
+main(options)
